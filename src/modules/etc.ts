6f7f49c1aa49308760b4fac94fc80ca5b1de44c3
Ron B <me@ronthecookie.me>
2020-08-02 04:13:57 +0300

    add !sheet

--- src/modules/etc.ts
+++ src/modules/etc.ts
@@ -15,4 +15,10 @@ export default class EtcModule extends Module {
 	async ask(msg: Message) {
 		await msg.channel.send("https://dontasktoask.com/");
 	}
+	@command({ aliases: ["spreadsheet", "voicelines", "vls", "vl"] })
+	async sheet(msg: Message) {
+		await msg.channel.send(
+			"Find voicelines to claim with `!claim`: https://docs.google.com/spreadsheets/d/1bS7cTsQ8vT9W7jhUgjIvTacFPy49WdOpQoihOaPj8KM/edit"
+		);
+	}
 }
