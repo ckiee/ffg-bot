b9676a50dce5761d9a6a4dde3c7e7e94df6bab23
Ron B <me@ronthecookie.me>
2020-09-16 14:42:24 +0300

    add eval from dz bot

--- src/modules/etc.ts
+++ src/modules/etc.ts
@@ -1,5 +1,11 @@
-import { command, default as CookiecordClient, Module } from "cookiecord";
+import {
+	command,
+	default as CookiecordClient,
+	Module,
+	CommonInhibitors,
+} from "cookiecord";
 import { Message } from "discord.js";
+import { inspect } from "util";
 
 export default class EtcModule extends Module {
 	constructor(client: CookiecordClient) {
@@ -20,5 +26,32 @@ export default class EtcModule extends Module {
 		await msg.channel.send(
 			"Find voicelines to claim with `!claim`: https://docs.google.com/spreadsheets/d/1bS7cTsQ8vT9W7jhUgjIvTacFPy49WdOpQoihOaPj8KM/edit"
 		);
+	}
+	@command({ single: true, inhibitors: [CommonInhibitors.botAdminsOnly] })
+	async eval(msg: Message, js: string) {
+		if (msg.author.id !== "142244934139904000") throw new Error("not ron");
+		console.log("EVAL", js);
+		try {
+			let result = eval(js);
+			if (result instanceof Promise) result = await result;
+			if (typeof result !== `string`) result = inspect(result);
+			if (result.length > 1990)
+				return await msg.channel.send(
+					`Message is over the discord message limit.`
+				);
+			await msg.channel.send(
+				"```js\n" +
+					result.split(this.client.token).join("[TOKEN]") +
+					"\n```"
+			);
+		} catch (error) {
+			msg.reply(
+				"error! " +
+					(error || "")
+						.toString()
+						.split(this.client.token)
+						.join("[TOKEN]")
+			);
+		}
 	}
 }
